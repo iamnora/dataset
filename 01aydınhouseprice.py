b5a296fd1589e4a9630bee9061a93d1ab86ee0c6
Feyza Ertürkmen <211805029@stu.adu.edu.tr>
2024-03-28 16:42:40 +0300

    bosluk_temizledim

--- 01aydınhouseprice.py
+++ 01aydınhouseprice.py
@@ -7,16 +7,12 @@ from stringClassifier import classify_strings
 
 data = pd.read_csv('aydınhouseprice_csv_file.csv', parse_dates=['Son Güncelleme Tarihi'])
 
-
 # Fiyat sütununu düzgün bir şekilde biçimlendirelim (TL işaretini kaldırıp sayıyı float'a dönüştürelim)
 data['Fiyat'] = data['Fiyat'].str.replace(' TL', '').str.replace('.', '').astype(float)
 
 data['Kira Getirisi'] = data['Kira Getirisi'].str.replace(' TL', '').str.replace('.', '').astype(float)
 
 data['Aidat'] = data['Aidat'].str.replace(' TL', '').str.replace('.', '').astype(float)
-
-
-
 
 
 # Güncelleme Tarihi sütununu tarih veri türüne dönüştürelim
@@ -38,18 +34,11 @@ data["Bina Yaşı"] = classify_strings(data["Bina Yaşı"])
 data["Cephe"] = classify_strings(data["Cephe"])
 
 
-
-
-
-
 # Kullanım Durumu sütunundaki kategorik değerleri sayısal değerlere dönüştürelim (0: Boş, 1: Kiracılı, 2: Mülk Sahibi)
 data['Kullanım Durumu'] = data['Kullanım Durumu'].map({'Boş': 0, 'Kiracılı': 1, 'Mülk Sahibi': 2})
 
 # Isınma Tipi ve Yakıt Tipi sütunlarını dummy değişkenlere dönüştürelim
 data = pd.get_dummies(data, columns=['Isınma Tipi', 'Yakıt Tipi'])
-
-
-
 
 # Yeni özellikler
 # Dolar, Euro, Altın, Petrol, Beton ve Demir sütunları arasında korelasyon olabilir, bu nedenle bu sütunlar arasında
@@ -60,10 +49,6 @@ data['Petrol:Dolar Oranı'] = data['Petrol'] / data['Dolar']
 data['Petrol:Euro Oranı'] = data['Petrol'] / data['Euro']
 data['Beton:Dolar Oranı'] = data['Beton'] / data['Dolar']
 data['Demir:Dolar Oranı'] = data['Demir'] / data['Dolar'] """
-
-
-
-
 
 # Giriş ve çıkış sütunlarını ayıralım
 X = data.drop(columns=['Fiyat'])
