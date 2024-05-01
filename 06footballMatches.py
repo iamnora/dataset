749fa7207f2d3df38a83c407a5d351e02a7481cb
PigeonTheDev <aycicek.u@gmail.com>
2024-05-01 17:14:19 +0300

    6_9_fixed

--- 06footballMatches.py
+++ 06footballMatches.py
@@ -1,10 +1,11 @@
 from matplotlib import pyplot as plt
 import pandas as pd
+from sklearn.metrics import r2_score
 from sklearn.model_selection import KFold, train_test_split, cross_validate, StratifiedKFold
 from sklearn.compose import make_column_selector as selector
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LogisticRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.metrics import accuracy_score
@@ -20,7 +21,7 @@ adult_census = pd.read_csv("footballmathces_v2.csv")
 selected_columns = [ 'home_team_data_squad_size', 'home_team_data_team_value', 'home_team_data_team_','away_team_data_squad_size', 'away_team_data_team_value', 'away_team_data_team_trophies']
 
 # Hedef değişkenler
-target_columns = ["Result", "Above_2.5"]
+target_columns = ["Result", "Above_2.5", "Total Goals"]
 
 # Önişleme
 categorical_preprocessor = OneHotEncoder(handle_unknown="ignore")
@@ -48,72 +49,105 @@ for target_col in target_columns:
 
     best_model = None
     best_score = 0
+    # Eğer output Regression ise
+    if(target_col == "Total Goals"):
+      # Modeli oluştur
+      model = make_pipeline(preprocessor, LinearRegression())
 
-    for model_name, model in models.items():
-        pipeline = make_pipeline(preprocessor, model)
+      # Veriyi eğitim ve test setlerine ayır
+      data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.2, random_state=47)
+
+      # Modeli eğit
+      _ = model.fit(data_train, target_train)
+
+      # Eğitim seti üzerinde tahminler yap
+      train_predictions = model.predict(data_train)
+
+      # Eğitim seti performansını ölç
+      train_r2 = r2_score(target_train, train_predictions)
+
+      # Test seti üzerinde tahminler yap
+      test_predictions = model.predict(data_test)
+
+      # Test seti performansını ölç
+      test_r2 = r2_score(target_test, test_predictions)
+
+      print("Eğitim R^2 skoru:", train_r2)
+      print("Test R^2 skoru:", test_r2)
+
+      joblib.dump(model, f"footballMatches_BestModel_{target_col}")
+    
+    #Eğer output Classification ise
+    else:
+         for model_name, model in models.items():
+
         
-        # Çapraz doğrulama
-        cv_results = cross_validate(pipeline, data, target, cv=StratifiedKFold(n_splits=10))
-        scores = cv_results["test_score"]
-        mean_accuracy = scores.mean()
-        std_accuracy = scores.std()
+             pipeline = make_pipeline(preprocessor, model)
         
-        print(f"{target_col} - {model_name} Cross Validation Accuracy: {mean_accuracy:.3f} +/- {std_accuracy:.3f}")
+         
+
+             # Çapraz doğrulama
+             cv_results = cross_validate(pipeline, data, target, cv=StratifiedKFold(n_splits=10))
+             scores = cv_results["test_score"]
+             mean_accuracy = scores.mean()
+             std_accuracy = scores.std()
         
-        # En iyi modeli seç
-        if mean_accuracy > best_score:
-            best_model = pipeline
-            best_score = mean_accuracy
+             print(f"{target_col} - {model_name} Cross Validation Accuracy: {mean_accuracy:.3f} +/- {std_accuracy:.3f}")
+          
+             # En iyi modeli seç
+             if mean_accuracy > best_score:
+                   best_model = pipeline
+                   best_score = mean_accuracy
 
-    # Train-test ayırma
-    data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.2, random_state=47, stratify=target)
+         # Train-test ayırma
+         data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.2, random_state=47, stratify=target)
 
-    # En iyi modelle eğitim
-    best_model.fit(data_train, target_train)
+         # En iyi modelle eğitim
+         best_model.fit(data_train, target_train)
 
-    # Eğitim seti performans metrikleri
-    train_predictions = best_model.predict(data_train)
-    train_accuracy = accuracy_score(target_train, train_predictions)
-    print(f"{target_col} - Train Accuracy:", train_accuracy)
+         # Eğitim seti performans metrikleri
+         train_predictions = best_model.predict(data_train)
+         train_accuracy = accuracy_score(target_train, train_predictions)
+         print(f"{target_col} - Train Accuracy:", train_accuracy)
 
-    # Test seti performans metrikleri
-    test_predictions = best_model.predict(data_test)
-    test_accuracy = accuracy_score(target_test, test_predictions)
-    print(f"{target_col} - Test Accuracy:", test_accuracy)
+          # Test seti performans metrikleri
+         test_predictions = best_model.predict(data_test)
+         test_accuracy = accuracy_score(target_test, test_predictions)
+         print(f"{target_col} - Test Accuracy:", test_accuracy)
 
-    # En iyi modeli kaydet
-    joblib.dump(best_model, f"footballMatches_BestModel_{target_col}")
+          # En iyi modeli kaydet
+         joblib.dump(best_model, f"footballMatches_BestModel_{target_col}")
 
  
 
 
 
 
-# Eğitim verisi histogramları
-plt.figure(figsize=(12, 6))
-for i, column in enumerate(data_train.columns):
-    plt.subplot(2, 3, i + 1)
-    sns.histplot(data_train[column], kde=True)
-    plt.title(column)
-plt.tight_layout()
-plt.show()
+ # Eğitim verisi histogramları
+    plt.figure(figsize=(12, 6))
+    for i, column in enumerate(data_train.columns):
+          plt.subplot(2, 3, i + 1)
+          sns.histplot(data_train[column], kde=True)
+          plt.title(column)
+    plt.tight_layout()
+    plt.show()
 
-# Eğitim verisi scatter plotları
-plt.figure(figsize=(12, 6))
-for i, column in enumerate(data_train.columns):
-    plt.subplot(2, 3, i + 1)
-    sns.scatterplot(x=target_train, y=data_train[column])
-    plt.title(column)
-plt.tight_layout()
-plt.show()
+    # Eğitim verisi scatter plotları
+    plt.figure(figsize=(12, 6))
+    for i, column in enumerate(data_train.columns):
+          plt.subplot(2, 3, i + 1)
+          sns.scatterplot(x=target_train, y=data_train[column])
+          plt.title(column)
+    plt.tight_layout()
+    plt.show()
 
 # Eğitim verisi violin plotları
-plt.figure(figsize=(12, 6))
-for i, column in enumerate(data_train.columns):
-    plt.subplot(2, 3, i + 1)
-    sns.violinplot(x=target_train, y=data_train[column])
-    plt.title(column)
-plt.tight_layout()
-plt.show()
+    plt.figure(figsize=(12, 6))
+    for i, column in enumerate(data_train.columns):
+          plt.subplot(2, 3, i + 1)
+          sns.violinplot(x=target_train, y=data_train[column])
+          plt.title(column)
+    plt.tight_layout()
+    plt.show()
 
 
--- 09music.py
+++ 09music.py
@@ -1,96 +1,91 @@
 import pandas as pd
-from sklearn.model_selection import train_test_split, cross_validate, KFold
+from sklearn.metrics import r2_score
+from sklearn.model_selection import train_test_split, cross_validate, StratifiedKFold
 from sklearn.compose import make_column_selector as selector
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.compose import ColumnTransformer
-from sklearn.linear_model import LinearRegression
+from sklearn.linear_model import LinearRegression, LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+import joblib
 from sklearn.pipeline import make_pipeline
-from sklearn.metrics import r2_score
-from sklearn.impute import SimpleImputer
 import matplotlib.pyplot as plt
 import seaborn as sns
-import joblib
 
 from stringClassifier import classify_strings
 
 # Veriyi yükle
 adult_census = pd.read_csv("Music Informations and Lyrics_ from Spotify and Musixmatch.csv")
 
-# Sadece istediğiniz sütunları seçin
-selected_columns = ["Artist","Artist Popularity","Artist Genres","Track Duration(ms)","Danceability","Energy","Key","Loudness","Acousticness","Liveness","Valence","Tempo"]
-data = adult_census[selected_columns]
-
-data["Artist"] = classify_strings(data["Artist"])
-data["Artist Genres"] = classify_strings(data["Artist Genres"])
+# Seçilen sütunlar
+selected_columns = ["Track Duration(ms)","Danceability","Energy","Key","Loudness","Acousticness","Liveness","Valence","Tempo"]
 
 
-# Hedef değişken
-target_name = "Track Popularity"
-target = adult_census[target_name]
+
+
+# Hedef değişkenler
+target_columns = ["Track Popularity","Artist Popularity"]
 
 # Önişleme
 categorical_preprocessor = OneHotEncoder(handle_unknown="ignore")
 numerical_preprocessor = StandardScaler()
 
-preprocessor = ColumnTransformer([
-    ('one-hot-encoder', categorical_preprocessor, selector(dtype_include="object")),
-    ('standard-scaler', numerical_preprocessor, selector(dtype_exclude="object"))
-])
-
-# Modeli oluştur
-model = make_pipeline(preprocessor, LinearRegression())
 
 
+# Her hedef değişken için ayrı ayrı modelleme yap
+for target_col in target_columns:
+    target = adult_census[target_col]
+    data = adult_census[selected_columns]
 
-# Veriyi eğitim ve test setlerine ayır
-data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.2, random_state=47)
+    preprocessor = ColumnTransformer([
+        ('one-hot-encoder', categorical_preprocessor, []),
+        ('standard-scaler', numerical_preprocessor, selected_columns)])
 
-# Modeli eğit
-_ = model.fit(data_train, target_train)
+  # Modeli oluştur
+    model = make_pipeline(preprocessor, LinearRegression())
 
-# Eğitim seti üzerinde tahminler yap
-train_predictions = model.predict(data_train)
+    
 
-# Eğitim seti performansını ölç
-train_r2 = r2_score(target_train, train_predictions)
+    # Train-test ayırma
+    data_train, data_test, target_train, target_test = train_test_split(data, target, test_size=0.2, random_state=47)
 
-# Test seti üzerinde tahminler yap
-test_predictions = model.predict(data_test)
+    # Modelle eğitim
+    model.fit(data_train, target_train)
 
-# Test seti performansını ölç
-test_r2 = r2_score(target_test, test_predictions)
+   # Modeli eğit
+    _ = model.fit(data_train, target_train)
+ 
+    #   Eğitim seti üzerinde tahminler yap
+    train_predictions = model.predict(data_train)
 
-print("Eğitim R^2 skoru:", train_r2)
-print("Test R^2 skoru:", test_r2)
+    # Eğitim seti performansını ölç
+    train_r2 = r2_score(target_train, train_predictions)
 
-# Modeli kaydet
-joblib.dump(model, "musicModel.pkl")
+    # Test seti üzerinde tahminler yap
+    test_predictions = model.predict(data_test)
 
-# Eğitim verisi histogramları
-plt.figure(figsize=(12, 6))
-for i, column in enumerate(data_train.columns):
-    plt.subplot(4, 4, i + 1)
-    sns.histplot(data_train[column], kde=True)
-    plt.title(column)
-plt.tight_layout()
-plt.show()
+    # Test seti performansını ölç
+    test_r2 = r2_score(target_test, test_predictions)
 
-# Eğitim verisi scatter plotları
-plt.figure(figsize=(12, 6))
-for i, column in enumerate(data_train.columns):
-    plt.subplot(4, 4, i + 1)
-    sns.scatterplot(x=target_train, y=data_train[column])
-    plt.title(column)
-plt.tight_layout()
-plt.show()
+    print("Eğitim R^2 skoru:", train_r2)
+    print("Test R^2 skoru:", test_r2)
 
-# Eğitim verisi violin plotları
-plt.figure(figsize=(12, 6))
-for i, column in enumerate(data_train.columns):
-    plt.subplot(4, 4, i + 1)
-    sns.violinplot(x=target_train, y=data_train[column])
-    plt.title(column)
-plt.tight_layout()
-plt.show()
+    # Modeli kaydet
+    joblib.dump(model, f"musicModal_{target_col}")
 
-
+    # Eğitim verisi scatter plotları
+    plt.figure(figsize=(12, 6))
+    for i, column in enumerate(data_train.columns):
+      plt.subplot(4, 4, i + 1)
+      sns.scatterplot(x=target_train, y=data_train[column])
+      plt.title(column)
+    plt.tight_layout()
+    plt.show()
+    # Eğitim verisi histogramlar
+    plt.figure(figsize=(12, 6))
+    for i, column in enumerate(data.columns):
+        plt.subplot(4, 4, i + 1)
+        sns.violinplot(x=target_train, y=data_train[column])
+        plt.title(column)
+    plt.tight_layout()
+    plt.show()
